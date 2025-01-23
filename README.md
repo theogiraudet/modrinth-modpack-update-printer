423b20fe57ce32530e9b12eada495c950c446c22
Théo Giraudet <theo.giraudet@orange.fr>
2025-01-23 19:35:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Modrinth Modpack Update Printer
+
+This action prints a report in an issue about if a modpack can be upgraded to the next Minecraft version.
+This action is meant to be used with the [Modrinth Modpack Update Checker](https://github.com/Gunivers/modrinth-modpack-update-checker) action, that checks if a modpack can be upgraded to the next Minecraft version according to its mods.
+The issue comment is only printed if the modpack can be upgraded and updated until all the mods are supported on the next Minecraft version.
+When the modpack can be upgraded, the action will also ping the person specified in the `person-to-ping` input.
+
+## Usage
+
+```yaml
+name: 'Modrinth Modpack Update Checker'
+
+on:
+  schedule:
+    - cron: '*/60 * * * *' # Every hours
+  workflow_dispatch: # Allow running the workflow manually
+
+jobs:
+  check-updates:
+    runs-on: ubuntu-latest
+    steps:
+        - name: 'Check Modrinth Modpack updates'
+            uses: Gunivers/modrinth-modpack-update-checker@v1.1.0
+            id: check
+            with:
+                modrinth-modpack-slug: 'example-modpack'
+
+        - name: 'Print Modrinth Modpack updates'
+            uses: Gunivers/modrinth-modpack-update-printer@v1.0.0
+            with:
+                current-mc-version: ${{ steps.check.outputs.current-mc-version }}
+                is-up-to-date: ${{ steps.check.outputs.is-up-to-date }}
+                tested-mc-version: ${{ steps.check.outputs.tested-mc-version }}
+                supported-mods: ${{ steps.check.outputs.supported-mods }}
+                unsupported-mods: ${{ steps.check.outputs.unsupported-mods }}
+                can-upgrade: ${{ steps.check.outputs.can-upgrade }}
+                person-to-ping: 'your-github-username'
+```
+
+## Inputs
+
+| Input | Description | Required | Supported since |
+| ----- | ----------- | -------- | --------------- |
+| `current-mc-version` | The current Minecraft version of the modpack | Yes | v1.0.0 |
+| `is-up-to-date` | Whether the modpack is up to date | Yes | v1.0.0 |
+| `tested-mc-version` | The Minecraft version that was tested | Yes | v1.0.0 |
+| `supported-mods` | The mods that are supported on the tested Minecraft version | Yes | v1.0.0 |
+| `unsupported-mods` | The mods that are not supported on the tested Minecraft version | Yes | v1.0.0 |
+| `can-upgrade` | Whether the modpack can be upgraded to the next Minecraft version | Yes | v1.0.0 |
+| `person-to-ping` | The person to ping in the issue when the modpack can be upgraded | Yes | v1.0.0 |
---
+++ main.ts
@@ -0,0 +1,98 @@
+import * as core from "@actions/core";
+import * as cache from "@actions/cache";
+import * as github from "@actions/github";
+import { Octokit } from "octokit";
+import fs from "node:fs";
+
+type Cache = {
+    issueNumber: number;
+    canUpgrade: boolean;
+    message: string;
+}
+
+const isUpToDate: boolean = core.getInput("is-up-to-date") === "true" || true;
+
+if(!isUpToDate) {
+
+    const currentMcVersion: string = core.getInput("current-mc-version");
+    const testedMcVersion: string = core.getInput("tested-mc-version");
+    const supportedMods: string[] = (core.getInput("supported-mods") || "").split(",");
+    const unsupportedMods: string[] = (core.getInput("unsupported-mods") || "").split(",");
+    const canUpgrade: boolean = core.getInput("can-upgrade") === "true";
+    const token: string = core.getInput("token");
+    const personToPing: string = core.getInput("person-to-ping");
+
+    let message = `
+    Current Minecraft Version: ${currentMcVersion}
+    Tested Minecraft Version: ${testedMcVersion}
+
+    Mods:
+    `;
+
+    const supportedModsList = supportedMods.map<[string, string]>((mod) => ["[x]", mod]);
+    const unsupportedModsList = unsupportedMods.map<[string, string]>((mod) => ["[ ]", mod]);
+
+    const modsList = [...supportedModsList, ...unsupportedModsList].sort((a, b) => a[1].localeCompare(b[1])).map((mod) => mod.join(" ")).join("\n");
+
+    message += modsList
+
+    const octokit = new Octokit({ auth: token });
+    await octokit.rest.users.getAuthenticated();
+
+    const cacheKey = "update-to-" + testedMcVersion;
+    const cacheFile = "./.cache/update-to-" + testedMcVersion + ".json";
+    const paths = ["./.cache/*.json"];
+
+    const hit = await cache.restoreCache(paths, cacheKey);
+
+    if (!hit) {
+        const result = await octokit.rest.issues.create({
+            owner: "TheoGaming",
+            repo: "Modrinth Modpack Update Checker",
+            title: "Update to Minecraft " + testedMcVersion,
+            body: message,
+        });
+        const number = result.data.id;
+
+        if(!fs.existsSync("./.cache")) {
+            fs.mkdirSync("./.cache");
+        }
+
+        fs.writeFileSync(cacheFile, JSON.stringify({
+            issueNumber: number,
+            canUpgrade: true,
+        }));
+
+        cache.saveCache(paths, cacheKey);
+    }
+
+    const cacheData: Cache = JSON.parse(fs.readFileSync(cacheFile, "utf8"));
+
+    if(!cacheData.canUpgrade) {
+        if(cacheData.message !== message) {
+            await octokit.rest.issues.update({
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                issue_number: cacheData.issueNumber,
+                body: message,
+            });
+        }
+
+        if(canUpgrade) {
+            await octokit.rest.issues.createComment({
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                issue_number: cacheData.issueNumber,
+                body: `@${personToPing} The modpack can be upgraded to Minecraft ${testedMcVersion}!`,
+            });
+        }
+
+        fs.writeFileSync(cacheFile, JSON.stringify({
+            issueNumber: cacheData.issueNumber,
+            canUpgrade: canUpgrade,
+            message: message,
+        }));
+
+        cache.saveCache(paths, cacheKey);
+    }
+}
