df449136890c235c3670486b32b771679d283f2f
Théo Giraudet <theo.giraudet@orange.fr>
2025-01-23 21:19:54 +0100

    Switch to npm

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM denoland/deno:latest as base
-
-WORKDIR /app
-
-COPY . ./
-
-RUN deno cache main.ts
-
-CMD ["run", "--allow-net", "--allow-env", "main.ts"]
--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import * as core from "@actions/core";
 import * as cache from "@actions/cache";
 import * as github from "@actions/github";
 import { Octokit } from "octokit";
-import fs from "node:fs";
+import fs from "fs";
 
 type Cache = {
     issueNumber: number;
@@ -10,89 +10,93 @@ type Cache = {
     message: string;
 }
 
-const isUpToDate: boolean = core.getInput("is-up-to-date") === "true" || true;
+main();
 
-if(!isUpToDate) {
+async function main() {
+    const isUpToDate: boolean = core.getInput("is-up-to-date") === "true" || true;
 
-    const currentMcVersion: string = core.getInput("current-mc-version");
-    const testedMcVersion: string = core.getInput("tested-mc-version");
-    const supportedMods: string[] = (core.getInput("supported-mods") || "").split(",");
-    const unsupportedMods: string[] = (core.getInput("unsupported-mods") || "").split(",");
-    const canUpgrade: boolean = core.getInput("can-upgrade") === "true";
-    const token: string = core.getInput("token");
-    const personToPing: string = core.getInput("person-to-ping");
+    if(!isUpToDate) {
 
-    let message = `
-    Current Minecraft Version: ${currentMcVersion}
-    Tested Minecraft Version: ${testedMcVersion}
+        const currentMcVersion: string = core.getInput("current-mc-version");
+        const testedMcVersion: string = core.getInput("tested-mc-version");
+        const supportedMods: string[] = (core.getInput("supported-mods") || "").split(",");
+        const unsupportedMods: string[] = (core.getInput("unsupported-mods") || "").split(",");
+        const canUpgrade: boolean = core.getInput("can-upgrade") === "true";
+        const token: string = core.getInput("token");
+        const personToPing: string = core.getInput("person-to-ping");
 
-    Mods:
-    `;
+        let message = `
+        Current Minecraft Version: ${currentMcVersion}
+        Tested Minecraft Version: ${testedMcVersion}
 
-    const supportedModsList = supportedMods.map<[string, string]>((mod) => ["[x]", mod]);
-    const unsupportedModsList = unsupportedMods.map<[string, string]>((mod) => ["[ ]", mod]);
+        Mods:
+        `;
 
-    const modsList = [...supportedModsList, ...unsupportedModsList].sort((a, b) => a[1].localeCompare(b[1])).map((mod) => mod.join(" ")).join("\n");
+        const supportedModsList = supportedMods.map<[string, string]>((mod) => ["[x]", mod]);
+        const unsupportedModsList = unsupportedMods.map<[string, string]>((mod) => ["[ ]", mod]);
 
-    message += modsList
+        const modsList = [...supportedModsList, ...unsupportedModsList].sort((a, b) => a[1].localeCompare(b[1])).map((mod) => mod.join(" ")).join("\n");
 
-    const octokit = new Octokit({ auth: token });
-    await octokit.rest.users.getAuthenticated();
+        message += modsList
 
-    const cacheKey = "update-to-" + testedMcVersion;
-    const cacheFile = "./.cache/update-to-" + testedMcVersion + ".json";
-    const paths = ["./.cache/*.json"];
+        const octokit = new Octokit({ auth: token });
+        await octokit.rest.users.getAuthenticated();
 
-    const hit = await cache.restoreCache(paths, cacheKey);
+        const cacheKey = "update-to-" + testedMcVersion;
+        const cacheFile = "./.cache/update-to-" + testedMcVersion + ".json";
+        const paths = ["./.cache/*.json"];
 
-    if (!hit) {
-        const result = await octokit.rest.issues.create({
-            owner: "TheoGaming",
-            repo: "Modrinth Modpack Update Checker",
-            title: "Update to Minecraft " + testedMcVersion,
-            body: message,
-        });
-        const number = result.data.id;
+        const hit = await cache.restoreCache(paths, cacheKey);
 
-        if(!fs.existsSync("./.cache")) {
-            fs.mkdirSync("./.cache");
-        }
-
-        fs.writeFileSync(cacheFile, JSON.stringify({
-            issueNumber: number,
-            canUpgrade: true,
-        }));
-
-        cache.saveCache(paths, cacheKey);
-    }
-
-    const cacheData: Cache = JSON.parse(fs.readFileSync(cacheFile, "utf8"));
-
-    if(!cacheData.canUpgrade) {
-        if(cacheData.message !== message) {
-            await octokit.rest.issues.update({
-                owner: github.context.repo.owner,
-                repo: github.context.repo.repo,
-                issue_number: cacheData.issueNumber,
+        if (!hit) {
+            const result = await octokit.rest.issues.create({
+                owner: "TheoGaming",
+                repo: "Modrinth Modpack Update Checker",
+                title: "Update to Minecraft " + testedMcVersion,
                 body: message,
             });
+            const number = result.data.id;
+
+            if(!fs.existsSync("./.cache")) {
+                fs.mkdirSync("./.cache");
+            }
+
+            fs.writeFileSync(cacheFile, JSON.stringify({
+                issueNumber: number,
+                canUpgrade: true,
+            }));
+
+            cache.saveCache(paths, cacheKey);
         }
 
-        if(canUpgrade) {
-            await octokit.rest.issues.createComment({
-                owner: github.context.repo.owner,
-                repo: github.context.repo.repo,
-                issue_number: cacheData.issueNumber,
-                body: `@${personToPing} The modpack can be upgraded to Minecraft ${testedMcVersion}!`,
-            });
+        const cacheData: Cache = JSON.parse(fs.readFileSync(cacheFile, "utf8"));
+
+        if(!cacheData.canUpgrade) {
+            if(cacheData.message !== message) {
+                await octokit.rest.issues.update({
+                    owner: github.context.repo.owner,
+                    repo: github.context.repo.repo,
+                    issue_number: cacheData.issueNumber,
+                    body: message,
+                });
+            }
+
+            if(canUpgrade) {
+                await octokit.rest.issues.createComment({
+                    owner: github.context.repo.owner,
+                    repo: github.context.repo.repo,
+                    issue_number: cacheData.issueNumber,
+                    body: `@${personToPing} The modpack can be upgraded to Minecraft ${testedMcVersion}!`,
+                });
+            }
+
+            fs.writeFileSync(cacheFile, JSON.stringify({
+                issueNumber: cacheData.issueNumber,
+                canUpgrade: canUpgrade,
+                message: message,
+            }));
+
+            cache.saveCache(paths, cacheKey);
         }
-
-        fs.writeFileSync(cacheFile, JSON.stringify({
-            issueNumber: cacheData.issueNumber,
-            canUpgrade: canUpgrade,
-            message: message,
-        }));
-
-        cache.saveCache(paths, cacheKey);
     }
 }
